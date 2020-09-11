fd88d69447bcc11fcf23fdcf065bfe8121d478b3
Nathan Carter <ncarter@bentley.edu>
2020-09-11 13:54:10 -0400

    Require user action in test launching

--- tests/test-server.js
+++ tests/test-server.js
@@ -2,7 +2,13 @@
 // Launch a simple web server in the test folder, so that the user can
 // point to it with their browser.
 
-require( './simple-server.js' ).startServer( { verbose : true } )
+require( './simple-server.js' ).startServer( { verbose : false } )
+
+/*
+
+Commenting out the following code so that it doesn't force the user
+to do this action.  Also, will be better if we later run this code
+from within a container, to let the user open the URL in the host.
 
 // Figure out which command is appropriate for opening URLs, based on
 // the user's platform.
@@ -14,3 +20,16 @@ const command = /^darwin/.test( process.platform ) ? 'open' :
 
 const { execSync } = require( 'child_process' )
 execSync( `${command} http://localhost:8080/tests/test-index.html` )
+
+*/
+
+console.log( `
++----------------------------------------------------+
+|                                                    |
+|   Server started.  Go here to view tests:          |
+|                                                    |
+|      http://localhost:8080/tests/test-index.html   |
+|                                                    |
++----------------------------------------------------+
+
+` )
