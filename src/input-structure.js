fb3a1f4712b85977ad89385b8db1139a26367ca9
Nathan Carter <ncarter@bentley.edu>
2020-09-24 13:41:42 -0400

    Add Input Structure file/class (empty)

---
+++ src/input-structure.js
@@ -0,0 +1,11 @@
+
+import { Structure } from '../src/structure.js'
+
+/**
+ * The InputStructure class, still just a stub for now.
+ */
+export class InputStructure extends Structure {
+    
+    static className = Structure.addSubclass( 'InputStructure', InputStructure )
+    
+}
---
+++ tests/input-structure-test.js
@@ -0,0 +1,10 @@
+
+import { InputStructure } from '../src/input-structure.js'
+
+describe( 'InputStructure', () => {
+
+    it( 'Ensure all expected global identifiers are declared', () => {
+        expect( InputStructure ).to.be.ok
+    } )
+
+} )
