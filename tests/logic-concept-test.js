94827ff8c30482164bd248ebf837b2bfb7cc75be
Nathan Carter <ncarter@bentley.edu>
2021-04-28 18:08:16 -0400

    Test LogicConcept constructor filtering

--- tests/logic-concept-test.js
+++ tests/logic-concept-test.js
@@ -1,6 +1,7 @@
 
 // We import this because it's the subject of this test suite.
 import { LogicConcept } from '../src/logic-concept.js'
+import { MathConcept } from '../src/math-concept.js'
 
 // Test suites begin here.
 
@@ -8,6 +9,38 @@ describe( 'LogicConcept module', () => {
 
     it( 'Ensure all expected global identifiers are declared', () => {
         expect( LogicConcept ).to.be.ok
+    } )
+
+    it( 'Ensure that we can construct LogicConcepts correctly', () => {
+        // Can we create an empty one?
+        let L = new LogicConcept()
+        expect( L ).to.be.ok
+        expect( L ).to.be.instanceOf( LogicConcept )
+        expect( L.numChildren() ).to.equal( 0 )
+        // Can we create a hierarchy of them?
+        L = new LogicConcept(
+            new LogicConcept(),
+            new LogicConcept(
+                new LogicConcept()
+            )
+        )
+        expect( L ).to.be.ok
+        expect( L ).to.be.instanceOf( LogicConcept )
+        expect( L.numChildren() ).to.equal( 2 )
+        expect( L.child( 0 ).numChildren() ).to.equal( 0 )
+        expect( L.child( 1 ).numChildren() ).to.equal( 1 )
+        // If we try to put non-LogicConcepts inside a LogicConcept,
+        // does it filter them out?
+        L = new LogicConcept(
+            new LogicConcept(),
+            new MathConcept(),
+            new LogicConcept()
+        )
+        expect( L ).to.be.ok
+        expect( L ).to.be.instanceOf( LogicConcept )
+        expect( L.numChildren() ).to.equal( 2 )
+        expect( L.child( 0 ) ).to.be.instanceOf( LogicConcept )
+        expect( L.child( 1 ) ).to.be.instanceOf( LogicConcept )
     } )
 
 } )
