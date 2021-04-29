535cea813cae5a38623346181365fb103bcece57
Nathan Carter <ncarter@bentley.edu>
2021-04-29 14:38:03 -0400

    Add tests for LogicConcept dirty flags

--- tests/logic-concept-test.js
+++ tests/logic-concept-test.js
@@ -7,11 +7,11 @@ import { MathConcept } from '../src/math-concept.js'
 
 describe( 'LogicConcept module', () => {
 
-    it( 'Ensure all expected global identifiers are declared', () => {
+    it( 'Should have all expected global identifiers declared', () => {
         expect( LogicConcept ).to.be.ok
     } )
 
-    it( 'Ensure that we can construct LogicConcepts correctly', () => {
+    it( 'Should correcty construct LogicConcepts', () => {
         // Can we create an empty one?
         let L = new LogicConcept()
         expect( L ).to.be.ok
@@ -41,6 +41,70 @@ describe( 'LogicConcept module', () => {
         expect( L.numChildren() ).to.equal( 2 )
         expect( L.child( 0 ) ).to.be.instanceOf( LogicConcept )
         expect( L.child( 1 ) ).to.be.instanceOf( LogicConcept )
+    } )
+
+} )
+
+describe( 'Dirty flags for LogicConcepts', () => {
+
+    it( 'Should be on by default', () => {
+        // make a small hierarchy
+        let L = new LogicConcept(
+            new LogicConcept(),
+            new LogicConcept()
+        )
+        // ensure all entries are dirty
+        expect( L.isDirty() ).to.equal( true )
+        expect( L.child( 0 ).isDirty() ).to.equal( true )
+        expect( L.child( 1 ).isDirty() ).to.equal( true )
+    } )
+
+    it( 'Should not propagate dirty changes upward', () => {
+        // make a small hierarchy
+        let L = new LogicConcept(
+            new LogicConcept(),
+            new LogicConcept()
+        )
+        // repeat previous test just as a sanity check
+        expect( L.isDirty() ).to.equal( true )
+        expect( L.child( 0 ).isDirty() ).to.equal( true )
+        expect( L.child( 1 ).isDirty() ).to.equal( true )
+        // change them one at a time to not dirty and ensure
+        // that they change only one at a time
+        L.child( 0 ).markDirty( false )
+        expect( L.isDirty() ).to.equal( true )
+        expect( L.child( 0 ).isDirty() ).to.equal( false )
+        expect( L.child( 1 ).isDirty() ).to.equal( true )
+        L.child( 1 ).markDirty( false )
+        expect( L.isDirty() ).to.equal( true )
+        expect( L.child( 0 ).isDirty() ).to.equal( false )
+        expect( L.child( 1 ).isDirty() ).to.equal( false )
+        L.markDirty( false )
+        expect( L.isDirty() ).to.equal( false )
+        expect( L.child( 0 ).isDirty() ).to.equal( false )
+        expect( L.child( 1 ).isDirty() ).to.equal( false )
+        // marking them dirty again also does not propagate
+        L.child( 0 ).markDirty( true )
+        expect( L.isDirty() ).to.equal( false )
+        expect( L.child( 0 ).isDirty() ).to.equal( true )
+        expect( L.child( 1 ).isDirty() ).to.equal( false )
+        L.child( 1 ).markDirty( true )
+        expect( L.isDirty() ).to.equal( false )
+        expect( L.child( 0 ).isDirty() ).to.equal( true )
+        expect( L.child( 1 ).isDirty() ).to.equal( true )
+        L.markDirty( true )
+        expect( L.isDirty() ).to.equal( true )
+        expect( L.child( 0 ).isDirty() ).to.equal( true )
+        expect( L.child( 1 ).isDirty() ).to.equal( true )
+        // unless you ask it to propagate
+        L.child( 0 ).markDirty( false, true )
+        expect( L.isDirty() ).to.equal( false )
+        expect( L.child( 0 ).isDirty() ).to.equal( false )
+        expect( L.child( 1 ).isDirty() ).to.equal( true )
+        L.child( 1 ).markDirty( true, true )
+        expect( L.isDirty() ).to.equal( true )
+        expect( L.child( 0 ).isDirty() ).to.equal( false )
+        expect( L.child( 1 ).isDirty() ).to.equal( true )
     } )
 
 } )
