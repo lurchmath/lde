c89bed5a322269c2ce318994e2a38c1e77bc5e0b
Nathan Carter <ncarter@bentley.edu>
2021-05-04 14:42:31 -0400

    Add Symbol test stub

---
+++ tests/symbol-test.js
@@ -0,0 +1,10 @@
+
+import { Symbol } from '../src/symbol.js'
+
+describe( 'Symbol', () => {
+
+    it( 'Should declare the relevant global identifiers', () => {
+        expect( Symbol ).to.be.ok
+    } )
+
+} )
