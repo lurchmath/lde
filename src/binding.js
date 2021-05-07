bf00cdd310e790bd0be8e8363d032d3a606f4ada
Nathan Carter <ncarter@bentley.edu>
2021-05-07 17:12:23 -0400

    Add Binding class with tests

---
+++ src/binding.js
@@ -0,0 +1,192 @@
+
+import { MathConcept } from './math-concept.js'
+import { Expression } from './expression.js'
+import { Symbol } from './symbol.js'
+
+/**
+ * Many mathematical expressions include "dummy variables," also called "bound
+ * variables."  For example, a summation from `i=1` to `n` uses `i` as a
+ * "dummy" or "bound" variable.  An integral ending in "dx" has x as a bound
+ * variable.  There are many other examples, including (but not limited to)
+ * the following.
+ * 
+ *  * indexed sums, products, unions, and intersections throughout mathematics
+ *  * for set-builder notation like `{ x+y | 0 < x < y }`
+ *  * existentially and universally quantified expressions, such as
+ *    "for all x > 0, |x|=x."
+ *  * lambda functions from computer science
+ * 
+ * We say that such expressions "bind" the dummy/bound variable, and we
+ * therefore call such expressions Bindings, and implement them in this class.
+ * 
+ * These were inspired by a structure of the same name defined in
+ * {@link https://openmath.org/about/ the OpenMath Standard}, but the reader
+ * is not expected to read that standard; we define in this documentation our
+ * version of what a Binding is.
+ * 
+ * A Binding is defined by a sequence of children, each of which is an
+ * {@link Expression}.  These children must meet specific requirements, given
+ * below.
+ * 
+ *  * The first child will be the operator or quantifier that binds the
+ *    variable(s).  (Note that a single operator can bind multiple variables,
+ *    such as when we say "for all x, y, and z in Q...")  This first child is
+ *    often called the "head."
+ *  * Following that operator will be the sequence of bound variables, all as
+ *    siblings of the operator, that is, children of the Binding.
+ *  * Following the bound variables will be exactly one final expression, the
+ *    "body."  This is what "sits inside" the operator, such as the integrand
+ *    in an integral, the summand in a summation, the inner statement in a
+ *    quantification, etc.
+ * 
+ * Consequently, a Binding will always have at least three children, because
+ * the head is required, at least one bound variable is required, and the body
+ * is required.  THe bound variables must all be {@link Symbol Symbols}.  The
+ * constructor defined below enforces this.
+ * 
+ * The head may be a single symbol (such as the universal quantifier) or it
+ * may be a compound expression (such as an integral with lower and upper
+ * limits, or a summation with lower and upper limits, or a union with an
+ * index set over which the union is to be taken, etc.).
+ * 
+ * This head {@link Expression} is not technically inside the scope of the
+ * Binding.  By this, we mean that any instance of the bound variables that
+ * appears in the head technically sits outside the Binding.  Although this
+ * would be extremely confusing notation, and thus is almost never relevant,
+ * one can imagine nested summations that both use the index `i`, where the
+ * bounds on the inner sum are written to include the `i` from the outer sum.
+ * There is a natural and sensible interpretation of such notation, even
+ * though we never use it in practice, because it is needlessly confusing.
+ */
+export class Binding extends Expression {
+    
+    static className = MathConcept.addSubclass( 'Binding', Binding )
+
+    /**
+     * Construct a new Binding instance from the given head (requried
+     * {@link Expression}), list of bound variables (one or more
+     * {@link Symbol Symbols}), and body (required {@link Expression}).  For
+     * example, to construct "for all x, -1 < x^2", you might do something
+     * like the following.  Let us assume that `forAll` is the universal
+     * quantifier (perhaps defined as a {@link Symbol}), `x` is the
+     * {@link Symbol} `x`, and we have symbols `lessThan` and `power`.
+     * 
+     * ```
+     * new Binding( forAll, x,
+     *     new Application( lessThan, Symbol( -1 ),
+     *         new Application( power, x, Symbol( 2 ) ) ) )
+     * ```
+     * 
+     * To construct the summation from `i=1` to `n` of `i^2`, we might do
+     * something like the following, assuming {@link Symbol Symbols} `i`,
+     * `n`, and `Sum`.  This might be read as "the sum from 1 to `n`, using
+     * bound variable `i`, of the formula `i^2`."
+     * 
+     * ```
+     * new Binding(
+     *     new Application( Sum, Symbol( 1 ), n ),
+     *     i,
+     *     new Application( power, i, Symbol( 2 ) )
+     * )
+     * ```
+     * 
+     * Note that while it is possible later to remove children from a Binding
+     * so that it does not have the required structure of head-variables-body,
+     * this is likely to result in the members of this class malfunctioning or
+     * throwing errors.  Clients should not remove the necessary children of a
+     * Binding; that is not supported.
+     * 
+     * Although it is nonsensical to repeat the same variable twice on the
+     * list of bound variables, we do not explicitly forbid it.
+     * 
+     * @param {Expression} head - The operator or quantifier that is being
+     *   used to bind variables in the context of the body expression.  This
+     *   argument is required, and must be an {@link Expression}.
+     * @param  {...Symbol} boundVars - The list of one or more variables that
+     *   this expression binds.  Any nonempty sequence of
+     *   {@link Symbol Symbols} is permitted; we make no distinction here
+     *   between what mathematicians often call variables vs. constants.
+     * @param {Expression} body - The interior of the Binding, whose meaning
+     *   varies based on the `head`.  In a summation, this is the summand;
+     *   in an integral, it is the integrand; etc.
+     * @throws This constructor throws an error if the first or last argument
+     *   (which will be interpreted as the head and body, respectively) are
+     *   not both {@link Expression Expressions}, or if any of the arguments
+     *   in between (the bound variables) are not {@link Symbol Symbols}, or
+     *   if the list of bound variables is empty.
+     */
+    constructor ( ...args ) {
+        const head = args.shift()
+        const body = args.pop()
+        const boundVars = args
+        if ( !( head instanceof Expression )
+          || !( body instanceof Expression ) )
+            throw new Error( 'The head and body in a Binding constructor '
+                           + 'must both be Expression instances' )
+        if ( boundVars.length === 0 )
+            throw new Error( 'A Binding constructor requires at least one '
+                           + 'bound variable' )
+        if ( !boundVars.every( v => v instanceof Symbol ) )
+            throw new Error( 'Every bound variable given to a Binding '
+                           + 'constructor must be a Symbol instance' )
+        super( head, ...boundVars, body )
+    }
+
+    /**
+     * Bindings store their head, bound variables, and body as children, in
+     * that order.  Thus this function returns the first child of this object,
+     * when considering it as an {@link Expression} (or more generally as a
+     * {@link MathConcept}).
+     * 
+     * If the children have not been manipulated since construction of the
+     * object, then this is the original head given at construction time.
+     * If a new first child has been inserted since then, or the old first
+     * child removed, the new first child is assumed to be the head.
+     * 
+     * @returns {Expression} the head that represents what type of Binding
+     *   structure this object is (e.g., quantifier, summation, etc.)
+     * @see {@link Binding#boundVariables boundVariables()}
+     * @see {@link Binding#body body()}
+     * @see {@link MathConcept#firstChild firstChild()}
+     */
+    head () { return this.firstChild() }
+
+    /**
+     * Bindings store their head, bound variables, and body as children, in
+     * that order.  Thus this function returns the last child of this object,
+     * when considering it as an {@link Expression} (or more generally as a
+     * {@link MathConcept}).
+     * 
+     * If the children have not been manipulated since construction of the
+     * object, then this is the original body given at construction time.
+     * If a new last child has been added since then, or the old last child
+     * removed, the new last child is assumed to be the body.
+     * 
+     * @returns {Expression} the body enclosed by this Binding expression
+     *   (e.g., the summand in a summation, etc.)
+     * @see {@link Binding#head head()}
+     * @see {@link Binding#boundVariables boundVariables()}
+     * @see {@link MathConcept#lastChild lastChild()}
+     */
+    body () { return this.lastChild() }
+
+    /**
+     * Bindings store their head, bound variables, and body as children, in
+     * that order.  Thus this function returns the list of children of this
+     * object, when considering it as an {@link Expression} (or more generally
+     * as a {@link MathConcept}), but excluding the first and last child.
+     * 
+     * If the children have not been manipulated since construction of the
+     * object, then this is the original list of bound variables given at
+     * construction time.  If they have been manipulated, then the new list of
+     * children, excluding the first and last, are assumed to be the list of
+     * bound variables.
+     * 
+     * @returns {...Symbol} the list of variables bound by this Binding
+     * @see {@link Binding#head head()}
+     * @see {@link Binding#body body()}
+     * @see {@link MathConcept#children children()}
+     */
+    boundVariables () { return this.children().slice( 1, -1 ) }
+
+}
---
+++ tests/binding-test.js
@@ -0,0 +1,105 @@
+
+import { LogicConcept } from '../src/logic-concept.js'
+import { Binding } from '../src/binding.js'
+import { Expression } from '../src/expression.js'
+import { Symbol } from '../src/symbol.js'
+
+describe( 'Binding', () => {
+
+    it( 'Should declare all relevant global identifiers', () => {
+        expect( Binding ).to.be.ok
+    } )
+
+    let f, x, y, E1, E2, E3, LC1, LC2
+    beforeEach( () => {
+        // Construct a few objects for use in the tests below.
+        f = new Symbol( 'f' )
+        x = new Symbol( 'x' )
+        y = new Symbol( 'y' )
+        E1 = new Expression
+        E2 = new Expression
+        E3 = new Expression
+        LC1 = new LogicConcept
+        LC2 = new LogicConcept( new LogicConcept )
+    } )
+
+    it( 'Should prevent construction with fewer than three arguments', () => {
+        // Try to construct Bindings with too few arguments,
+        // and verify that an error is thrown in each case.
+        expect( () => { new Binding } ).to.throw()
+        expect( () => { new Binding( f ) } ).to.throw()
+        expect( () => { new Binding( x, y ) } ).to.throw()
+        expect( () => { new Binding( E1, E2 ) } ).to.throw()
+        expect( () => { new Binding( LC1, LC2 ) } ).to.throw()
+    } )
+
+    it( 'Should prevent construction with wrong argument types', () => {
+        // Try to construct Bindings with non-Expressions as the head,
+        // but all other parameters following the construction rules,
+        // and verify that an error is thrown in each case.
+        expect( () => { new Binding( LC1, x, E1 ) } ).to.throw()
+        expect( () => { new Binding( LC2, x, y, E2 ) } ).to.throw()
+        // Try to construct Bindings with non-Expressions as the body,
+        // but all other parameters following the construction rules,
+        // and verify that an error is thrown in each case.
+        expect( () => { new Binding( E1, x, LC1 ) } ).to.throw()
+        expect( () => { new Binding( E2, x, y, LC2 ) } ).to.throw()
+        // Try to construct Bindings with non-Symbols as the bound variables,
+        // but all other parameters following the construction rules,
+        // and verify that an error is thrown in each case.
+        expect( () => { new Binding( E1, LC1, E2 ) } ).to.throw()
+        expect( () => { new Binding( E2, E2, x, y, E3 ) } ).to.throw()
+        expect( () => { new Binding( E2, x, E2, y, E3 ) } ).to.throw()
+        expect( () => { new Binding( E2, x, y, E2, E3 ) } ).to.throw()
+        // Try to construct Bindings no bound variables,
+        // but all other parameters following the construction rules,
+        // and verify that an error is thrown in each case.
+        expect( () => { new Binding( E1, E2 ) } ).to.throw()
+        expect( () => { new Binding( E2, E3 ) } ).to.throw()
+        expect( () => { new Binding( E1, E3 ) } ).to.throw()
+    } )
+
+    it( 'Should permit construction with correct argument types', () => {
+        // Try to construct Binding instances with Expressions as the head and
+        // body, and Symbols as the bound variables (of which there are >0),
+        // and verify that no error is thrown in each case.
+        expect( () => { new Binding( E1, x, E2 ) } ).not.to.throw()
+        expect( () => { new Binding( E2, f, y, E3 ) } ).not.to.throw()
+        expect( () => { new Binding( E1, f, x, y, E3 ) } ).not.to.throw()
+    } )
+
+    it( 'Should return head, boundVars, and body correctly', () => {
+        // Construct Bindings as in earlier tests.
+        // Note the importance of making copies when creating children!
+        // Otherwise you remove the same instance from earlier parents.
+        let B1 = new Binding( E1, x, E2 )
+        let B2 = new Binding( E2.copy(), f, y, E3 )
+        let B3 = new Binding(
+            E1.copy(), f.copy(), x.copy(), y.copy(), E3.copy() )
+        // compute head in each case
+        expect( B1.head() ).to.equal( E1 )
+        expect( B2.head() ).to.equal( B2.child(0) )
+        expect( B3.head() ).to.equal( B3.child(0) )
+        // compute boundVars in each case
+        let tmp = B1.boundVariables()
+        expect( tmp ).to.be.instanceof( Array )
+        expect( tmp.length ).to.equal( 1 )
+        expect( tmp[0] ).to.equal( x )
+        tmp = B2.boundVariables()
+        expect( tmp ).to.be.instanceof( Array )
+        expect( tmp.length ).to.equal( 2 )
+        expect( tmp[0] ).to.equal( B2.child(1) )
+        expect( tmp[1] ).to.equal( y )
+        tmp = B3.boundVariables()
+        expect( tmp ).to.be.instanceof( Array )
+        expect( tmp.length ).to.equal( 3 )
+        expect( tmp[0] ).to.equal( B3.child(1) )
+        expect( tmp[1] ).to.equal( B3.child(2) )
+        expect( tmp[2] ).to.equal( B3.child(3) )
+        // compute head in each case
+        expect( B1.body() ).to.equal( E2 )
+        expect( B2.body() ).to.equal( E3 )
+        expect( B3.body() ).to.equal( B3.child(4) )
+    } )
+
+} )
