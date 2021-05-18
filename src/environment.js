d8f389d01645c6f15ed8cdb19cac4fad866f717b
Nathan Carter <ncarter@bentley.edu>
2021-05-18 15:35:25 -0400

    Add conclusions() member, with tests

--- src/environment.js
+++ src/environment.js
@@ -1,6 +1,8 @@
 
 import { MathConcept } from './math-concept.js'
 import { LogicConcept } from './logic-concept.js'
+import { Expression } from './expression.js'
+import { Declaration } from './declaration.js'
 
 /**
  * The Environment class, still just a stub for now.
@@ -10,5 +12,31 @@ import { LogicConcept } from './logic-concept.js'
 export class Environment extends LogicConcept {
     
     static className = MathConcept.addSubclass( 'Environment', Environment )
-    
+
+    /**
+     * Any {@link LogicConcept} can be marked as a "given," meaning that it is
+     * to be seen as a hypothesis/assumption within its context.  Any
+     * {@link LogicConcept} not so marked is viewed as a "claim," meaning that
+     * it is an assertion/statement of truth, within its context.  For this,
+     * we use the attribute functions built into the {@link MathConcept}
+     * class, including {@link MathConcept#isA isA()},
+     * {@link MathConcept#asA asA()}, and so on.
+     * 
+     * The conclusions of a LogicConcept include:
+     * 
+     *  * all of its children that are claim {@link Expression Expressions}
+     *  * all of the conclusions in any of its child Environments, recursively
+     */
+    conclusions () {
+        let result = [ ]
+        this.children().forEach( child => {
+            if ( child.isA( 'given' ) ) return
+            if ( child instanceof Expression || child instanceof Declaration )
+                result.push( child ) // guaranteed to be outermost expr/decl
+            else if ( child instanceof Environment )
+                result = result.concat( child.conclusions() )
+        } )
+        return result
+    }
+
 }
--- tests/environment-test.js
+++ tests/environment-test.js
@@ -1,5 +1,6 @@
 
 import { Environment } from '../src/environment.js'
+import { Symbol } from '../src/symbol.js'
 
 describe( 'Environment', () => {
 
@@ -7,4 +8,198 @@ describe( 'Environment', () => {
         expect( Environment ).to.be.ok
     } )
 
+} )
+
+///////////////
+//
+//  NOTE:
+//
+//  Some tests in this module have been copied over from old code,
+//  then commented out until they're needed in the future.
+//
+///////////////
+
+describe( 'Conclusions', () => {
+
+    let A, B, C, D, E, _A, _B, _C, _D, _E
+
+    beforeEach( () => {
+        A = new Symbol( 'A' )
+        B = new Symbol( 'B' )
+        C = new Symbol( 'C' )
+        D = new Symbol( 'D' )
+        E = ( ...args ) => new Environment( ...args )
+        _A = new Symbol( 'A' ).asA( 'given' )
+        _B = new Symbol( 'B' ).asA( 'given' )
+        _C = new Symbol( 'C' ).asA( 'given' )
+        _D = new Symbol( 'D' ).asA( 'given' )
+        _E = ( ...args ) => E( ...args ).makeIntoA( 'given' )
+    } )
+  
+    it( 'should say the conclusions in { A } are only A', () => {
+        let X = E( A )
+        // expect( X.toString() ).to.be( '{ A }' )
+        let Y = X.conclusions()
+        expect( Y ).to.have.length( 1 )
+        expect( Y ).to.contain( A )
+        // expect( A.isAConclusionIn( X ) ).to.be( true )
+    } )
+  
+    it( 'should say the conclusions in { A B } are A and B', () => {
+        let X = E( A, B )
+        // expect( X.toString() ).to.be( '{ A B }' )
+        let Y = X.conclusions()
+        expect( Y ).to.have.length( 2 )
+        expect( Y ).to.contain( A )
+        expect( Y ).to.contain( B )
+        // expect( A.isAConclusionIn( X ) ).to.be( true )
+        // expect( B.isAConclusionIn( X ) ).to.be( true )
+    } )
+  
+    it( 'should say the conclusions in { A { B } } are A and B', () => {
+        let X = E( A, E( B ) )
+        // expect( X.toString() ).to.be( '{ A { B } }' )
+        let Y = X.conclusions()
+        expect( Y ).to.have.length( 2 )
+        expect( Y ).to.contain( A )
+        expect( Y ).to.contain( B )
+        // expect( A.isAConclusionIn( X ) ).to.be( true )
+        // expect( B.isAConclusionIn( X ) ).to.be( true )
+    } )
+  
+    it( 'should say the conclusions in :{ A B } are A and B', () => {
+        let X = _E( A, B )
+        // expect( X.toString() ).to.be( ':{ A B }' )
+        let Y = X.conclusions()
+        expect( Y ).to.have.length( 2 )
+        expect( Y ).to.contain( A )
+        expect( Y ).to.contain( B )
+        // expect( A.isAConclusionIn( X ) ).to.be( true )
+        // expect( B.isAConclusionIn( X ) ).to.be( true )
+    } )
+  
+    it( 'should say the conclusions in :{ :A B } are only B', () => {
+        let X = _E( _A, B )
+        // expect( X.toString() ).to.be( ':{ :A B }' )
+        let Y = X.conclusions()
+        expect( Y ).to.have.length( 1 )
+        expect( Y ).to.contain( B )
+        // expect( _A.isAConclusionIn( X ) ).to.be( false )
+        // expect( B.isAConclusionIn( X ) ).to.be( true )
+    } )
+  
+    it( 'should say the conclusions in :{ A :B } are only A', () => {
+        let X = _E( A, _B )
+        // expect( X.toString() ).to.be( ':{ A :B }' )
+        let Y = X.conclusions()
+        expect( Y ).to.have.length( 1 )
+        expect( Y ).to.contain( A )
+        // expect( A.isAConclusionIn( X ) ).to.be( true )
+        // expect( _B.isAConclusionIn( X ) ).to.be( false )
+    } )
+  
+    it( 'should say there are no conclusions in :{ :A :B }', () => {
+        let X = _E( _A, _B )
+        // expect( X.toString() ).to.be( ':{ :A :B }' )
+        let Y = X.conclusions()
+        expect( Y ).to.eql( [ ] )
+        // expect( _A.isAConclusionIn( X ) ).to.be( false )
+        // expect( _B.isAConclusionIn( X ) ).to.be( false )
+    } )
+  
+    it( 'should say the conclusions in { { A } } are only A', () => {
+        let X = E( E( A ) )
+        // expect( X.toString() ).to.be( '{ { A } }' )
+        let Y = X.conclusions()
+        expect( Y ).to.have.length( 1 )
+        expect( Y ).to.contain( A )
+        // expect( A.isAConclusionIn( X ) ).to.be( true )
+        // expect( A.parent().isAConclusionIn( X ) ).to.be( false )
+    } )
+  
+    it( 'should say there are no conclusions in { :{ A } }', () => {
+        let X = E( _E( A ) )
+        // expect( X.toString() ).to.be( '{ :{ A } }' )
+        let Y = X.conclusions()
+        expect( Y ).to.eql( [ ] )
+        // expect( A.isAConclusionIn( X ) ).to.be( false )
+        // expect( A.parent().isAConclusionIn( X ) ).to.be( false )
+    } )
+  
+    it( 'should say there are no conclusions in { { :A } }', () => {
+        let X = E( E( _A ) )
+        // expect( X.toString() ).to.be( '{ { :A } }' )
+        let Y = X.conclusions()
+        expect( Y ).to.be.eql( [ ] )
+        // expect( _A.isAConclusionIn( X ) ).to.be( false )
+        // expect( _A.parent().isAConclusionIn( X ) ).to.be( false )
+    } )
+  
+    it( 'should say the conclusions in { { :A B } } are only B', () => {
+        let X = E( E( _A, B ) )
+        // expect( X.toString() ).to.be( '{ { :A B } }' )
+        let Y = X.conclusions()
+        expect( Y ).to.have.length( 1 )
+        expect( Y ).to.contain( B )
+        // expect( _A.isAConclusionIn( X ) ).to.be( false )
+        // expect( _A.parent().isAConclusionIn( X ) ).to.be( false )
+        // expect( B.isAConclusionIn( X ) ).to.be( true )
+    } )
+  
+    it( 'should say the conclusions in { { :A B } C } are B and C', () => {
+        let X = E( E( _A, B ), C )
+        // expect( X.toString() ).to.be( '{ { :A B } C }' )
+        let Y = X.conclusions()
+        expect( Y ).to.have.length( 2 )
+        expect( Y ).to.contain( B )
+        expect( Y ).to.contain( C )
+        // expect( _A.isAConclusionIn( X ) ).to.be( false )
+        // expect( _A.parent().isAConclusionIn( X ) ).to.be( false )
+        // expect( B.isAConclusionIn( X ) ).to.be( true )
+        // expect( C.isAConclusionIn( X ) ).to.be( true )
+    } )
+  
+    it( 'should say the conclusions in { :{ :A B } C } are only C', () => {
+        let X = E( _E( _A, B ), C )
+        // expect( X.toString() ).to.be( '{ :{ :A B } C }' )
+        let Y = X.conclusions()
+        expect( Y ).to.have.length( 1 )
+        expect( Y ).to.contain( C )
+        // expect( _A.isAConclusionIn( X ) ).to.be( false )
+        // expect( _A.parent().isAConclusionIn( X ) ).to.be( false )
+        // expect( B.isAConclusionIn( X ) ).to.be( false )
+        // expect( C.isAConclusionIn( X ) ).to.be( true )
+    } )
+  
+    it( 'should say the conclusions in { { { :A B } :{ :C D } } :D } are only B', () => {
+        let X = E( E( E( _A, B ), _E( _C, D ) ), _D )
+        // expect( X.toString() ).to.be( '{ { { :A B } :{ :C D } } :D }' )
+        let Y = X.conclusions()
+        expect( Y ).to.have.length( 1 )
+        expect( Y ).to.contain( B )
+        // expect( _A.isAConclusionIn( X ) ).to.be( false )
+        // expect( _A.parent().isAConclusionIn( X ) ).to.be( false )
+        // expect( B.isAConclusionIn( X ) ).to.be( true )
+        // expect( _C.isAConclusionIn( X ) ).to.be( false )
+        // expect( _C.parent().isAConclusionIn( X ) ).to.be( false )
+        // expect( D.isAConclusionIn( X ) ).to.be( false )
+        // expect( _D.isAConclusionIn( X ) ).to.be( false )
+    } )
+  
+    it( 'should say the conclusions in { { :{ :A B } { C :D } } D } are C and D', () => {
+        let X = E( E( _E( _A, B ), E( C, _D ) ), D )
+        // expect( X.toString() ).to.be( '{ { :{ :A B } { C :D } } D }' )
+        let Y = X.conclusions()
+        expect( Y ).to.have.length( 2 )
+        expect( Y ).to.contain( C )
+        expect( Y ).to.contain( D )
+        // expect( _A.isAConclusionIn( X ) ).to.be( false )
+        // expect( _A.parent().isAConclusionIn( X ) ).to.be( false )
+        // expect( B.isAConclusionIn( X ) ).to.be( false )
+        // expect( C.isAConclusionIn( X ) ).to.be( true )
+        // expect( C.parent().isAConclusionIn( X ) ).to.be( false )
+        // expect( _D.isAConclusionIn( X ) ).to.be( false )
+        // expect( D.isAConclusionIn( X ) ).to.be( true )
+    } )
+  
 } )
