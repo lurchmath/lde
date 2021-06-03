b4ec2b81f41bd8c1c646151658318e42051d523d
Nathan Carter <ncarter@bentley.edu>
2021-06-03 13:25:36 -0400

    Add Declaration class with all its tests

--- src/declaration.js
+++ src/declaration.js
@@ -1,14 +1,193 @@
 
 import { MathConcept } from './math-concept.js'
 import { LogicConcept } from './logic-concept.js'
+import { Symbol as LurchSymbol } from './symbol.js'
+import { Expression } from './expression.js'
 
 /**
- * The Declaration class, still just a stub for now.
+ * In mathematics, we declare new variables and constants regularly, when we
+ * write text such as, "Let $n$ be any integer," or "So let $k$ be the unique
+ * value making $f(k)=t-1$ hold."  We call these Declarations.
  * 
- * Documentation coming later.
+ * They come in two types:
+ * 
+ *  * A variable declaration is the kind where the declared symbols are to be
+ *    used as variables, typically at the beginning of a subproof that will
+ *    justify a universal statement.  For instance, "Let $x\in A$ be
+ *    arbitrary."
+ *  * A constant declaration is the kind where the declared symbols are not
+ *    intended to vary in meaning throughout the subsequent discussion.  These
+ *    symbols are often global in scope and are typically inappropriate for
+ *    use in quantifiers.  For instance, the declaration of the symbol 0 in
+ *    the Peano axioms is a constant declaration.
+ * 
+ * Each Declaration instance has a type (variable or constant), a list of
+ * symbols being declared (which are instances of the {@link Symbol Symbol}
+ * class), and optionally a body that stipulates some fact about the symbols
+ * that is being stated by the declaration.  For instance, in the declaration
+ * "Let $x\in A$ be arbitrary," the type is a variable declaration, the list
+ * of symbols contains just one entry, $x$, and the body is the statement
+ * $x\in A$ being made about $x$.
  */
 export class Declaration extends LogicConcept {
     
     static className = MathConcept.addSubclass( 'Declaration', Declaration )
-    
+
+    /**
+     * A class-level constant that functions like an enum.
+     * When constructing a Declaration, pass as the first argument either
+     * this value or {@link Declaration.Constant the Constant type} instead.
+     * 
+     * Passing this as the declaration type indicates that the constructed
+     * Declaration declares one or more variables, rather than one or more
+     * constants.
+     * 
+     * Example: `new Declaration( Declaration.Variable, x )`
+     */
+    static Variable = Symbol.for( 'Variable' )
+    /**
+     * A class-level constant that functions like an enum.
+     * When constructing a Declaration, pass as the first argument either
+     * this value or {@link Declaration.Variable the Variable type} instead.
+     * 
+     * Passing this as the declaration type indicates that the constructed
+     * Declaration declares one or more constants, rather than one or more
+     * variables.
+     * 
+     * Example: `new Declaration( Declaration.Constant, pi )`
+     */
+    static Constant = Symbol.for( 'Constant' )
+    /**
+     * An array containing all valid options for the first parameter to the
+     * Declaration constructor, collecting all the constants in the
+     * makeshift enum, including:
+     * 
+     *  * {@link Declaration.Variable Variable}
+     *  * {@link Declaration.Constant Constant}
+     */
+    static Types = [
+        Declaration.Variable,
+        Declaration.Constant
+    ]
+
+    /**
+     * Construct a variable or constant declaration.  You must provide a flag
+     * as the first argument indicating the type of declaration
+     * ({@link Declaration.Variable variable} or
+     * {@link Declaration.Constant constant}), and then the second argument
+     * must be either the {@link Symbol Symbol} to be declared or an array of
+     * one or more {@link Symbol Symbols} to be declared.  The optional final
+     * argument is the body of the declaration.
+     * 
+     * In mathematics, we often declare variables or constants with certain
+     * conditions, such as "Let $x$ be any real number greater than $N$."  In
+     * that sentence, we are declaring a variable $x$ and imposing on it a
+     * two-part condition (a conjunction), that $x$ is a real number and it is
+     * greater than $N$.  Thus the body of the declaration would be that
+     * mathematical expression, $x\in\mathbb{R}\wedge x>N$.
+     * 
+     * However, it is also possible to declare variables with no conditions
+     * attached, as in the mathematical statement "Let $x$ be arbitrary."  So
+     * the body of a declaration is optional.
+     * 
+     * The resulting Declaration instance will have as its children list the
+     * list of symbols it declares followed by the body, if a body is present.
+     * 
+     * @param {Symbol} type - Must be one of the valid types listed in the
+     *   array {@link Declaration.Types Declaration.Types}, indicating whether
+     *   this declaration is intended to declare variables or constants.  Note
+     *   that this must be a JavaScript `Symbol` instance, not an instance of
+     *   the {@link Symbol Symbol} class defined in this repository.  See
+     *   {@link Declaration.Variable Variable} and
+     *   {@link Declaration.Constant Constant} for example calls to this
+     *   constructor using those values.
+     * @param {Symbol|Symbol[]} symbols - Either a single
+     *   {@link Symbol Symbol} instance or an array of Symbol instances.  Note
+     *   that this must be a Symbol in the sense of the class defined in this
+     *   repository, rather than the standard JavaScript Symbol type.  If a
+     *   single symbol is passed, it will be treated as an array of just one
+     *   symbol.
+     * @param {Expression} [body] - An optional body of the declaration, as
+     *   documented above.
+
+     * @see {@link Declaration#type type()}
+     * @see {@link Declaration#symbols symbols()}
+     * @see {@link Declaration#expression expression()}
+    */
+    constructor ( type, symbols, body ) {
+        if ( arguments.length > 3 )
+            throw 'Too many arguments to Declaration constructor: '
+                + `expected 3, got ${arguments.length}`
+        if ( !Declaration.Types.includes( type ) )
+            throw `Invalid declaration type: ${String(type)}`
+        if ( symbols instanceof LurchSymbol ) symbols = [ symbols ]
+        if ( !( symbols instanceof Array ) )
+            throw 'Second argument to Declaration constructor must be '
+                + 'either a Symbol or an array'
+        if ( !symbols.every( symbol => symbol instanceof LurchSymbol ) )
+            throw 'Not every entry in the array given to the '
+                + 'Declaration constructor was a Symbol'
+        if ( body && !( body instanceof Expression ) )
+            throw 'Optional third parameter to Declaration constructor, '
+                + 'if provided, must be an Expression instance'
+        if ( body )
+            super( ...symbols, body )
+        else
+            super( ...symbols )
+        this._body = body
+        this.setAttribute( 'declaration type', Symbol.keyFor( type ) )
+    }
+
+    /**
+     * Return the type of declaration, which is the first argument passed to
+     * the constructor.  It will be one of the items in
+     * {@link Declaration.Types the list of types}.
+     * 
+     * @returns {Symbol} A JavaScript Symbol (not a Lurch
+     *   {@link Symbol Symbol} instance) representing the
+     *   {@link Declaration.Types type of declaration}, such as variable or
+     *   constant
+     * @see {@link Declaration#symbols symbols()}
+     * @see {@link Declaration#expression expression()}
+     */
+    type () { return Symbol.for( this.getAttribute( 'declaration type' ) ) }
+
+    /**
+     * Return the array of {@link Symbol Symbols} provided to this object at
+     * construction time.  This will be of length at least one, since the
+     * constructor requires there to be at least one symbol.  The only
+     * exception to this rule is that Declarations, once constructed, can be
+     * modified by removing their children, but this creates invalid forms,
+     * so clients should not do so.
+     * 
+     * @returns {Symbol[]} An array of {@link Symbol Symbol} instances, those
+     *   symbols being declared by this object
+     * @see {@link Declaration#type type()}
+     * @see {@link Declaration#expression expression()}
+     */
+    symbols () {
+        return this._body ? this.allButLastChild() : this.children()
+    }
+
+    /**
+     * Returns the body of this declaration.  For an explanation of the
+     * meaning of a declaration body, see
+     * {@link Declaration the documentation for the constructor}.
+     *
+     * In those cases where the Declaration has a body, it will also be the
+     * Declaration's last child.  If the body has been deleted by modifying
+     * this Declaration after its construction, this function returns
+     * undefined.
+     * 
+     * @returns {Expression|undefined} The body expression provided to this
+     *   object at construction time, if there was one, or undefined if not.
+     *   If there was one, it is guaranteed to be an instance of the
+     *   {@link Expression Expression} class.
+     * @see {@link Declaration#type type()}
+     * @see {@link Declaration#symbols symbols()}
+     */
+    expression () {
+        return this._body == this.lastChild() ? this._body : undefined
+    }
+
 }
--- tests/declaration-test.js
+++ tests/declaration-test.js
@@ -1,10 +1,221 @@
 
 import { Declaration } from '../src/declaration.js'
+import { Expression } from '../src/expression.js'
+import { Symbol as LurchSymbol } from '../src/symbol.js'
+import { LogicConcept } from '../src/logic-concept.js'
+import { Environment } from '../src/environment.js'
 
 describe( 'Declaration', () => {
 
-    it( 'Ensure all expected global identifiers are declared', () => {
+    it( 'Should declare the expected global identifiers', () => {
         expect( Declaration ).to.be.ok
+        console.log( Declaration.Types )
+    } )
+
+    let x, y, z, E1, E2
+    beforeEach( () => {
+        // Make some (Lurch) symbols to declare
+        // Note: Most places in the codebase, we call these Symbols, but in
+        // those rare circumstances where we are using JavaScript Symbols
+        // also, we import these as "LurchSymbols" instead.  (See the import
+        // statements at the top of this file).
+        x = new LurchSymbol( 'x' )
+        y = new LurchSymbol( 'y' )
+        z = new LurchSymbol( 'z' )
+        // Make some expressions to use as bodies
+        E1 = new Expression( new Expression, new Expression )
+        E2 = new Expression( new Expression )
+    } )
+
+    it( 'Should notice if we call it with the wrong signature', () => {
+        // This test is just of a convenience feature.  You could imagine
+        // designing the Declaration constructor in either of two ways:
+        // Declaration(type,[sym1,...,symN],body) or
+        // Declaration(type,sym1,...,symN,body).
+        // We chose the former because the latter is ambiguous.  (Is
+        // Declaration(type,x,y) a declaration of x and y with no body or a
+        // declaration of x with body y?)
+        // But because it's very common to accidentally use the other syntax
+        // (to which JavaScript will respond by silently dropping all
+        // arguments after the third one--oy) we introduced an error message
+        // if the caller sends >3 arguments.  We test here that it works.
+        expect( () => {
+            new Declaration( Declaration.Variable, x, y, z )
+        } ).to.throw( /^Too many arguments to Declaration constructor: / )
+    } )
+
+    it( 'Should permit Variable Declarations', () => {
+        expect( () => {
+            const D = new Declaration( Declaration.Variable, x )
+        } ).not.to.throw()
+        expect( () => {
+            const D = new Declaration( Declaration.Variable, [ x, y ] )
+        } ).not.to.throw()
+        expect( () => {
+            const D = new Declaration( Declaration.Variable, x, E1 )
+        } ).not.to.throw()
+        expect( () => {
+            const D = new Declaration( Declaration.Variable, [ x, y ], E2 )
+        } ).not.to.throw()
+    } )
+
+    it( 'Should permit Constant Declarations', () => {
+        expect( () => {
+            const D = new Declaration( Declaration.Constant, x )
+        } ).not.to.throw()
+        expect( () => {
+            const D = new Declaration( Declaration.Constant, [ x, y ] )
+        } ).not.to.throw()
+        expect( () => {
+            const D = new Declaration( Declaration.Constant, x, E1 )
+        } ).not.to.throw()
+        expect( () => {
+            const D = new Declaration( Declaration.Constant, [ x, y ], E2 )
+        } ).not.to.throw()
+    } )
+
+    it( 'Should not permit any other type of Declarations', () => {
+        expect( () => {
+            const D = new Declaration( Symbol.for( 'example' ), x )
+        } ).to.throw( /^Invalid declaration type: / )
+        expect( () => {
+            const D = new Declaration( undefined, [ x, y ] )
+        } ).to.throw( /^Invalid declaration type: / )
+        expect( () => {
+            const D = new Declaration( 'Variable', x, E1 )
+        } ).to.throw( /^Invalid declaration type: / )
+        expect( () => {
+            const D = new Declaration( 10, [ x, y ], E2 )
+        } ).to.throw( /^Invalid declaration type: / )
+    } )
+
+    it( 'Should return the correct type for each Declaration', () => {
+        // make two declarations
+        const D1 = new Declaration( Declaration.Variable, x )
+        const D2 = new Declaration( Declaration.Constant, x, E1 )
+        // first, can we query the types?
+        expect( D1.type() ).to.equal( Declaration.Variable )
+        expect( D2.type() ).to.equal( Declaration.Constant )
+        // second, are the types readable?
+        expect( String( D1.type() ) ).to.match( /variable/i )
+        expect( String( D2.type() ) ).to.match( /constant/i )
+        // third, are the types comparable correctly with one another?
+        const D3 = new Declaration( Declaration.Variable, y )
+        const D4 = new Declaration( Declaration.Constant, y, E2 )
+        expect( D1.type() ).to.equal( D3.type() )
+        expect( D2.type() ).to.equal( D4.type() )
+        expect( D1.type() ).not.to.equal( D2.type() )
+        expect( D1.type() ).not.to.equal( D4.type() )
+        expect( D2.type() ).not.to.equal( D1.type() )
+        expect( D2.type() ).not.to.equal( D3.type() )
+    } )
+
+    it( 'Should permit Declarations of one, two, or more Symbols', () => {
+        expect( () => {
+            const D = new Declaration( Declaration.Variable, x )
+        } ).not.to.throw()
+        expect( () => {
+            const D = new Declaration( Declaration.Variable, [ x, y ], E1 )
+        } ).not.to.throw()
+        expect( () => {
+            const D = new Declaration( Declaration.Constant, [ x, y, z ] )
+        } ).not.to.throw()
+        expect( () => {
+            const D = new Declaration( Declaration.Constant, [ x, y, z ], E2 )
+        } ).not.to.throw()
+    } )
+
+    it( 'Should not permit Declarations with zero symbols', () => {
+        expect( () => {
+            const D = new Declaration( Declaration.Variable )
+        } ).to.throw( /^Second argument to Declaration constructor / )
+        expect( () => {
+            const D = new Declaration( Declaration.Constant, E1 )
+        } ).to.throw( /^Second argument to Declaration constructor / )
+    } )
+
+    it( 'Should not permit Declarations of non-symbols', () => {
+        expect( () => {
+            const D = new Declaration( Declaration.Variable, E1, E2 )
+        } ).to.throw( /^Second argument to Declaration constructor must be / )
+        expect( () => {
+            const D = new Declaration( Declaration.Constant, [ x, y, E2 ], E1 )
+        } ).to.throw( /^Not every entry in the array.*was a Symbol$/ )
+    } )
+
+    it( 'Should let us query the symbol list from a declaration', () => {
+        const D1 = new Declaration( Declaration.Variable, x )
+        let list = D1.symbols()
+        expect( list.length ).to.equal( 1 )
+        expect( list[0] ).to.equal( x )
+        const D2 = new Declaration( Declaration.Variable, [ x, y ], E1 )
+        list = D2.symbols()
+        expect( list.length ).to.equal( 2 )
+        expect( list[0] ).to.equal( x )
+        expect( list[1] ).to.equal( y )
+        const D3 = new Declaration( Declaration.Constant, [ x, y, z ] )
+        list = D3.symbols()
+        expect( list.length ).to.equal( 3 )
+        expect( list[0] ).to.equal( x )
+        expect( list[1] ).to.equal( y )
+        const D4 = new Declaration( Declaration.Constant, [ z, y, x ], E2 )
+        list = D4.symbols()
+        expect( list.length ).to.equal( 3 )
+        expect( list[0] ).to.equal( z )
+        expect( list[1] ).to.equal( y )
+        expect( list[2] ).to.equal( x )
+    } )
+
+    it( 'Should let us construct Declarations with or without bodies', () => {
+        // with bodies
+        expect( () => {
+            const D = new Declaration( Declaration.Variable, x, E1 )
+        } ).not.to.throw()
+        expect( () => {
+            const D = new Declaration( Declaration.Constant, [ x, y ], E2 )
+        } ).not.to.throw()
+        // without bodies
+        expect( () => {
+            const D = new Declaration( Declaration.Variable, x )
+        } ).not.to.throw()
+        expect( () => {
+            const D = new Declaration( Declaration.Constant, [ x, y ] )
+        } ).not.to.throw()
+    } )
+
+    it( 'Should restrict Declaration bodies to Expressions only', () => {
+        // can't be a generic LogicConcept
+        expect( () => {
+            const D = new Declaration( Declaration.Variable, x,
+                new LogicConcept )
+        } ).to.throw(
+            /^Optional third parameter.*must be an Expression instance/ )
+        // can't be another Declaration
+        expect( () => {
+            const D = new Declaration( Declaration.Constant, x,
+                new Declaration( Declaration.Constant, y ) )
+        } ).to.throw(
+            /^Optional third parameter.*must be an Expression instance/ )
+        // can't be an Environment
+        expect( () => {
+            const D = new Declaration( Declaration.Variable, x,
+                new Environment )
+        } ).to.throw(
+            /^Optional third parameter.*must be an Expression instance/ )
+    } )
+
+    it( 'Should let us query the body of any Declaration', () => {
+        // body == E1
+        const D1 = new Declaration( Declaration.Variable, x, E1 )
+        expect( D1.expression() ).to.equal( E1 )
+        // body == E2
+        const D2 = new Declaration( Declaration.Constant, [ x, y ], E2 )
+        expect( D2.expression() ).to.equal( E2 )
+        // no body provided at construction time
+        const D3 = new Declaration( Declaration.Variable, x )
+        expect( D3.expression() ).to.equal( undefined )
+        const D4 = new Declaration( Declaration.Constant, [ x, y ] )
+        expect( D4.expression() ).to.equal( undefined )
     } )
 
 } )
