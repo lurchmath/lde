ee38809e733fce09b4d336f0f65ebb8d57b0b534
Nathan Carter <ncarter@bentley.edu>
2021-06-03 15:53:14 -0400

    Add missing docs to Environment class

--- src/environment.js
+++ src/environment.js
@@ -5,9 +5,22 @@ import { Expression } from './expression.js'
 import { Declaration } from './declaration.js'
 
 /**
- * The Environment class, still just a stub for now.
+ * An Environment in Lurch is a wrapper that can enclose any sequence of
+ * {@link LogicConcept LogicConcepts}, and creates a variable scope in doing
+ * so.  This can be useful for many purposes, including:
  * 
- * Documentation coming later.
+ *  * Delimiting exercises in a text from the rest of the text, so that
+ *    variables declared in those exercises do not remain in scope for the
+ *    rest of the text.
+ *  * Nesting subproofs within a proof so that it is clear where assumptions
+ *    and variable declarations are (and are not) in force.
+ *  * Marking the boundaries of theorems, proofs, definitions, axioms, and
+ *    other important structures that form a single, cohesive unit.
+ * 
+ * This class inherits its constructor from its parent class; to create an
+ * environment wrapping a sequence `L_1` through `L_n` of
+ * {@link LogicConcept LogicConcepts}, just call
+ * `new Environment( L1, ..., Ln )`.
  */
 export class Environment extends LogicConcept {
     
