c9bd52d17ca6746c623bc5509643e3d37e5a348c
Nathan Carter <ncarter@bentley.edu>
2021-09-23 10:27:34 -0400

    Creating Matching module, uniting matching subfolder

---
+++ src/matching.js
@@ -0,0 +1,44 @@
+
+/**
+ * @module Matching
+ * 
+ * This module simply gathers all the definitions from smaller modules in the
+ * `/src/matching` folder and places them in a single namespace.  This makes it
+ * convenient for the user to `import Matching from './matching.js'` and then
+ * be able to refer to a `Matching.Constraint` or `Matching.Problem`, rather
+ * than needing to import modules that are in subfolders, and pollute the global
+ * namespace with generic terms like `Constraint` and `Problem`.
+ * 
+ * The full list of imported classes and modules is given below.  The members of
+ * the {@link module:ExpressionFunctions ExpressionFunctions module} are each
+ * imported directly, so that the client can refer to them as, for example,
+ * `Matching.newEF`, rather than `Matching.ExpressionFunctions.newEF`, which
+ * would be too cumbersome.
+ * 
+ * @see {@link NewSymbolStream NewSymbolStream class}
+ * @see {@link Constraint Constraint class}
+ * @see {@link CaptureConstraint CaptureConstraint class}
+ * @see {@link CaptureConstraints CaptureConstraints class}
+ * @see {@link module:ExpressionFunctions ExpressionFunctions module}
+ * @see {@link Problem Problem class}
+ */
+
+import { NewSymbolStream } from "./matching/new-symbol-stream.js"
+import { Constraint, metavariable } from "./matching/constraint.js"
+import {
+    CaptureConstraint, CaptureConstraints
+} from "./matching/capture-constraint.js"
+import {
+    newEF, isAnEF, arityOfEF, applyEF, constantEF, projectionEF, applicationEF,
+    newEFA, isAnEFA, canBetaReduce, betaReduce, fullBetaReduce
+} from '../src/matching/expression-functions.js'
+import { Problem } from "./matching/problem.js"
+
+export default {
+    NewSymbolStream,
+    Constraint, metavariable,
+    CaptureConstraint, CaptureConstraints,
+    newEF, isAnEF, arityOfEF, applyEF, constantEF, projectionEF, applicationEF,
+    newEFA, isAnEFA, canBetaReduce, betaReduce, fullBetaReduce,
+    Problem
+}
