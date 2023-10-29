ff1202d7f3fa3052b52ac1c57f794af3ccd29c24
Ken Monks <ken.monks@gmail.com>
2023-10-29 13:57:13 -0400

    BIH vs Cases test.

--- src/experimental/initproofs.js
+++ src/experimental/initproofs.js
@@ -8,6 +8,50 @@ process.stdout.write(defaultPen(`Loading proofs ...\n`))
 let start = Date.now()
 ////////////////////////////////////////////////////////////////////////////
 
+doc = $(
+`
+{
+  Declare or
+  Rules:
+  { 
+    cases> :{ :W or V  :{:W U}  :{:V U}  U }  
+    :{ :W   W or V   V or W } 
+  }   
+  { :P or Q
+    { :P  Q or P}
+    { :Q  Q or P}
+    Q or P by cases
+  } 
+}
+`)
+doc = interpret(doc)
+validate(doc)
+
+bih = $(
+    `
+    {
+      Declare or
+      Rules:
+      { 
+        :{ :W or V  :{:W U}  :{:V U}  U }  
+        :{ :W   W or V   V or W } 
+      } 
+      Recall
+      { :P or Q
+        :{ :P  Q or P}
+        :{ :Q  Q or P}
+        Q or P 
+      } 
+      { :P or Q
+        { :P  Q or P}
+        { :Q  Q or P}
+        Q or P 
+      }
+    }
+    `)
+bih = interpret(bih)
+validate(bih)
+    
 
 // Accumulator = { totaltime:0, numcalls:0, numsolns:0, numlines: 0}
 ///////////////////////////////////////////////////////////
