0c57eaef8af02186a80fe23974ae7b325bb9cc40
Ken Monks <ken.monks@gmail.com>
2024-04-08 17:34:07 -0400

    Removing extraneous comments.

--- src/experimental/global-validation.js
+++ src/experimental/global-validation.js
@@ -143,46 +143,6 @@ const time = (description) => { if (Debug) console.time(description) }
 const timeEnd = (description) => { if (Debug) console.timeEnd(description) }
 ////////////////////////////////////////////////////////////////////////////////
 
-
-/**
- * We just use a global for storing the current options. This avoids having to
- * pass it as an optional argument through the entire validation chain of
- * interpretations and validation tools. To set an option just do e.g.
- * `LurchOptions.avoidLoneMetavars = false`. Current validation options are:
- *
- *   * `validateall` - if true, validate all inferences.  If false, only
- *      validate the target.
- *   * `checkPreemies` - Check for preemies iff this is true
- *   * `processBIHs` - process BIHs iff this is true         
- *   * `avoidLoneMetavars` - if true don't try to instantiate lone
- *      metavariables, otherwise try to instantiate them with every user
- *      proposition.
- *   * `avoidLoneEFAs` - the same thing for lone EFAs        
- *   * `processEquations` - process equations iff this is true  
- *   * `processCases`- process the cases tool iff this is true      
- *   * `autoCases` - similar to avoidLoneMetavars=false. If true, then identify
- *      all Cases-like rules and try to instantiate their univar conclusion with
- *      every user's conclusion in the document.
- *   * `processCAS` - process CAS tool iff this is true
- *
- * @see {@link validate validate()}
- */
-// const LurchOptions = { 
-//   validateall:true ,    
-//   checkPreemies:true ,  
-//   processBIHs:true ,
-//   avoidLoneMetavars:true ,
-//   avoidLoneEFAs:true ,    
-//   processEquations:true ,    
-//   processCases:true ,    
-//   autoCases:false ,
-//   processCAS:true ,
-//   swapTheoremProofPairs: true ,
-//   updateProgress: async () => { }  ,
-//   updateFreq: 100 ,
-//   badResultMsg: 'indeterminate' 
-// }
-
 /**
  *
  * ## Validate!
